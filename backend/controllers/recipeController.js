8a8618f86aac9b8aa0feb95ff2b96a48addb3f7a
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-22 17:09:58 -0600

    fixed file path issues

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,19 +1,21 @@
-const asyncHandler = require("express-async-handler");
 const fs = require("fs");
+
+const filePath = "./backend/data.json";
 
 // @desc    Get recipes
 // @route   GET /
 // @access  Private
 const getRecipes = (req, res, next) => {
-    let recipe;
+  let recipe;
 
-    try {
-        const jsonString = fs.readFileSync('./data.json')
-        recipe = JSON.parse(jsonString)
-      } catch(err) {
-        console.log(err)
-        return
-      }
+  try {
+    const jsonString = fs.readFileSync(filePath);
+    recipe = JSON.parse(jsonString);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  console.log(recipe);
 
   res.json(recipe);
 };
