9b2ac8d0875e038fa0461c05ea57a004cab66be6
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-22 19:52:33 -0600

    Step 2

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -6,19 +6,19 @@ const filePath = "./backend/data.json";
 // @route   GET /recipes
 // @access  Private
 const getRecipes = (req, res, next) => {
-  let recipe;
+  let jsonData;
   let recipeNames = [];
 
   try {
     const jsonString = fs.readFileSync(filePath);
-    recipe = JSON.parse(jsonString);
+    jsonData = JSON.parse(jsonString);
   } catch (err) {
     console.log(err);
     return;
   }
 
-  for(let i=0; i<recipe.recipes.length; i++){
-    recipeNames.push(recipe.recipes[i].name)
+  for(let i=0; i<jsonData.recipes.length; i++){
+    recipeNames.push(jsonData.recipes[i].name)
   }
   
   const names = { "recipeNames": recipeNames };
@@ -26,6 +26,35 @@ const getRecipes = (req, res, next) => {
   res.json(names);
 };
 
+// @desc    Get recipe details
+// @route   GET /recipes/details/:name
+// @access  Private
+const getDetails = (req, res, next) => {
+    let jsonData;
+    let ingredients;
+    let numSteps;
+    let details = {};
+  
+    try {
+      const jsonString = fs.readFileSync(filePath);
+      jsonData = JSON.parse(jsonString);
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  
+    for(let i=0; i<jsonData.recipes.length; i++){
+      if(jsonData.recipes[i].name === req.params.name){
+        ingredients = jsonData.recipes[i].ingredients;
+        numSteps = jsonData.recipes[i].instructions.length;
+        details = { "details": { "ingredients": ingredients, "numSteps": numSteps } };
+      }
+    } 
+  
+    res.json(details);
+  };
+
 module.exports = {
   getRecipes,
+  getDetails,
 };
--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {
   getRecipes,
+  getDetails,
 } = require("../controllers/recipeController");
 
 router.get("/recipes", getRecipes);
+router.get("/recipes/details/:name", getDetails);
 
 module.exports = router;
