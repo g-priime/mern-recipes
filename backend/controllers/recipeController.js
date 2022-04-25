9b23c8aa366c352a09f38091aec86a8973bf939a
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-25 12:22:48 -0600

    Part 3 working

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -17,11 +17,11 @@ const getRecipes = (req, res, next) => {
     return;
   }
 
-  for(let i=0; i<jsonData.recipes.length; i++){
-    recipeNames.push(jsonData.recipes[i].name)
+  for (let i = 0; i < jsonData.recipes.length; i++) {
+    recipeNames.push(jsonData.recipes[i].name);
   }
-  
-  const names = { "recipeNames": recipeNames };
+
+  const names = { recipeNames: recipeNames };
 
   res.json(names);
 };
@@ -30,31 +30,75 @@ const getRecipes = (req, res, next) => {
 // @route   GET /recipes/details/:name
 // @access  Private
 const getDetails = (req, res, next) => {
-    let jsonData;
-    let ingredients;
-    let numSteps;
-    let details = {};
-  
-    try {
-      const jsonString = fs.readFileSync(filePath);
-      jsonData = JSON.parse(jsonString);
-    } catch (err) {
-      console.log(err);
+  let jsonData;
+  let ingredients;
+  let numSteps;
+  let details = {};
+
+  try {
+    const jsonString = fs.readFileSync(filePath);
+    jsonData = JSON.parse(jsonString);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  for (let i = 0; i < jsonData.recipes.length; i++) {
+    if (jsonData.recipes[i].name === req.params.name) {
+      ingredients = jsonData.recipes[i].ingredients;
+      numSteps = jsonData.recipes[i].instructions.length;
+      details = { details: { ingredients: ingredients, numSteps: numSteps } };
+    }
+  }
+
+  res.json(details);
+};
+
+// @desc    Post recipes
+// @route   POST /recipes
+// @access  Private
+const postRecipes = (req, res, next) => {
+  const recipe = {
+    name: "butteredBagel",
+    ingredients: ["1 bagel", "butter"],
+    instructions: ["cut the bagel", "spread butter on bagel"],
+  };
+
+  let jsonData;
+
+  try {
+    const jsonString = fs.readFileSync(filePath);
+    jsonData = JSON.parse(jsonString);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  for (let i = 0; i < jsonData.recipes.length; i++) {
+    if (jsonData.recipes[i].name === recipe.name) {
+      res.status(400).json({
+        error: "Recipe already exists",
+      });
       return;
     }
-  
-    for(let i=0; i<jsonData.recipes.length; i++){
-      if(jsonData.recipes[i].name === req.params.name){
-        ingredients = jsonData.recipes[i].ingredients;
-        numSteps = jsonData.recipes[i].instructions.length;
-        details = { "details": { "ingredients": ingredients, "numSteps": numSteps } };
-      }
-    } 
-  
-    res.json(details);
-  };
+  }
+
+  jsonData.recipes.push(recipe);
+
+  const jsonString = JSON.stringify(jsonData);
+  fs.writeFile(filePath, jsonString, (err) => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file");
+    }
+  });
+
+  res.status(201).json();
+};
 
 module.exports = {
   getRecipes,
   getDetails,
+  postRecipes,
 };
--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getRecipes,
   getDetails,
+  postRecipes,
 } = require("../controllers/recipeController");
 
 router.get("/recipes", getRecipes);
 router.get("/recipes/details/:name", getDetails);
+router.post("/recipes", postRecipes);
 
 module.exports = router;
