302216ed1d0358b4b79b6edce71e3fae429bb4f7
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-27 11:58:03 -0600

    changed server port

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/recipeRoutes"));
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(port, () => {
+ console.log(`Server started on port ${port}`);
 });
